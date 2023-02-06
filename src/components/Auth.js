5cc84b0c394f20060263b2c07bc4df215c5d6557
verhaq <haqverdah98@gmail.com>
2023-02-06 10:32:25 -0600

    fixed login bug

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,63 +1,67 @@
-import {useState} from 'react'
+import {useState, useContext} from 'react'
 import axios from 'axios'
-import { useContext } from 'react'
-import AuthContext from '../store/authContext'
- 
-const Auth = () => {
 
-    
+import AuthContext from '../store/authContext'
+const Auth = () => {
+    const [register, setRegister] = useState(true)
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [register, setRegister] = useState(true)
-    
-    const authCtx = useContext(AuthContext)
-   
-   const submitHandler = e => {
-       e.preventDefault()
-       
-       const body = {
-           username,
-           password
-        }
-        
-        const baseUrl = "https://socialmtn.devmountain.com"
+    const [message, setMessage] = useState('')
+    const [display, setDisplay] = useState('none')
 
-       axios.post(register ? `${baseUrl}/register` : `${baseUrl}/login`, body)
-        .then((res) => {
-            console.log('AFTER AUTH', res.data)
-            authCtx.login(res.data.token, res.data.expiration, res.data.id)
-        })
-        .catch(err => {
-            setPassword('')
-            setUsername('')
-        })
-        
-        console.log('submitHandler called')
-   }
- 
-   return (
-       <main>
-           <h1>Welcome!</h1>
-           <form className='form auth-form' onSubmit={submitHandler}>
-               <input
-                type='text'
-                placeholder='Username'
-                value={username} 
-                onChange={e => setUsername(e.target.value)}
-                   className='form-input'/>
-               <input
-                 type="text"
-                placeholder='Password'
-               value={password}
-               onChange={e => setPassword(e.target.value)}
-                   className='form-input'/>
-               <button className='form-btn'>
-                   {register ? 'Sign Up' : 'Login'}
-               </button>
-           </form>
-           <button className='form-btn'>Need to {register ? 'Login' : 'Sign Up'}?</button>
-       </main>
-   )
+    const authCtx = useContext(AuthContext)
+
+    const submitHandler = e => {
+        e.preventDefault()
+
+        setDisplay('none')
+
+        const body = {
+            username,
+            password
+        }
+
+        const url = 'https://socialmtn.devmountain.com'
+
+        axios.post(register ? `${url}/register` : `${url}/login`, body)
+            .then((res) => {
+                console.log('AFTER AUTH', res.data)
+                authCtx.login(res.data.token, res.data.exp, res.data.userId)
+            })
+            .catch(err => {
+                setMessage(err.response.data)
+                setDisplay('block')
+                setPassword('')
+                setUsername('')
+            })
+    }
+
+    return (
+        <main>
+            <h1>Welcome!</h1>
+            <form className='form auth-form' onSubmit={submitHandler}>
+                <input 
+                    type='text' 
+                    placeholder='username' 
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    className='form-input'/>
+                <input 
+                    type='password' 
+                    placeholder='password' 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    className='form-input'/>
+                <button className='form-btn'>
+                    {register ? 'Sign Up' : 'Login'}
+                </button>
+            </form>
+            <p style={{display: display}} className='auth-msg'>{message}</p>
+            <button className='form-btn' onClick={() => setRegister(!register)}>
+                Need to {register ? 'Login' : 'Sign Up'}?
+            </button>
+        </main>
+    )
 }
- 
+
 export default Auth
